b4f51a0d3fff3811964b8d4b85ed938470b1b1eb
opsJson <54485405+opsJson@users.noreply.github.com>
2022-07-12 16:43:49 -0300

    Update client.c

--- client.c
+++ client.c
@@ -3,14 +3,22 @@
 int main(void) {
 
 	char buf[2048] = {0};
+	char *request =
+		"GET / HTTP/1.1\r\n"
+		"Host: www.google.com"
+		"\r\n"
+		""
+		"\r\n";
 
 	int sock = csocket_connect("google.com", "80");
 
-	csocket_write(sock, "GET / HTTP/1.1\r\n\r\n");
+	csocket_write(sock, request, strlen(request));
 
 	csocket_read(sock, buf, sizeof(buf));
 
 	printf("%s\n", buf);
+	
+	csocket_close(sock);
 
 	return 0;
 }
