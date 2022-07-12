29a04c6edd5c8a068dc7e92ff21e2668290e51bb
opsJson <54485405+opsJson@users.noreply.github.com>
2022-07-12 16:44:55 -0300

    Update server.c

--- server.c
+++ server.c
@@ -1,17 +1,19 @@
 #include "./csocket.h"
 
 bool onRequest(int sock) {
+	
 	char buffer[2048] = {0};
+	char *response =
+		"HTTP/1.1 200 OK\r\n"
+		"Content-Length: 5\r\n\r\n"
+		"hello!\r\n"
+		"\r\n";
 
 	csocket_read(sock, buffer, sizeof(buffer));
 
 	printf("%s\n", buffer);
 
-	csocket_write(sock,
-		"HTTP/1.1 200 OK\r\n"
-		"Content-Length: 5\r\n\r\n"
-		"hello!\r\n"
-		"\r\n");
+	csocket_write(sock, response, strlen(response));
 	
 	return true;
 }
